6124ff47a49817f090e1a9c2758e7098d4fd0016
tondanov <toneknovotny@gmail.com>
2024-04-15 10:46:15 +0200

    new project

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import live from "./live.svg"
+import live from "./live.svg";
 
 import css from "./tech/css.png";
 import html from "./tech/html.png";
@@ -11,10 +11,10 @@ import javascript from "./tech/javascript.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
 
-
 import cvapp from "./cvapp.png";
 import eshop from "./eshop.png";
 import searchbar from "./searchbar.png";
+import lectureeasily from "./lectureeasily.png";
 
 export {
   logo,
@@ -31,4 +31,5 @@ export {
   searchbar,
   eshop,
   live,
+  lectureeasily,
 };
